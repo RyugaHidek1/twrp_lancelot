4e14ea42c30852495506a42372bd19b0db8057da
Stepan <81905872+RyugaHidek1@users.noreply.github.com>
2023-08-31 22:04:15 +0300

    Move part to device.mk

--- twrp_lancelot.mk
+++ twrp_lancelot.mk
@@ -14,24 +14,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from lancelot device
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
-# API
-PRODUCT_TARGET_VNDK_VERSION := 33
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-# FastbootD
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
-
-# Health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.dm_default_key.options_format.version=2
-
 PRODUCT_DEVICE := lancelot
 PRODUCT_NAME := twrp_lancelot
 PRODUCT_BRAND := Redmi
